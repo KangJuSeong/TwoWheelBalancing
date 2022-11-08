35f909ffdeea09df579b2e58aa37704d05db3827
KangJuSeong <wntjd3686@gmail.com>
2022-11-08 15:41:22 +0900

    modify: self.bus in GyroDriver.py

--- GyroDriver.py
+++ GyroDriver.py
@@ -16,18 +16,18 @@ class Gyro:
     GYRO_YOUT_H  = 0x45
     GYRO_ZOUT_H  = 0x47
     Device_Address = 0x68
-
+    self.bus = smbus.SMBus(1) 
+    	
     def __init__(self):
-        bus = smbus.SMBus(1) 	
-    	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-    	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-    	bus.write_byte_data(Device_Address, CONFIG, 0)
-    	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-    	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+    	self.bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+    	self.bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+    	self.bus.write_byte_data(Device_Address, CONFIG, 0)
+    	self.bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+    	self.bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
     def read_raw_data(self, addr):
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
+        high = self.bus.read_byte_data(Device_Address, addr)
+        low = self.bus.read_byte_data(Device_Address, addr+1)
         
         value = ((high << 8) | low)
             
