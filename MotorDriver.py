2a52861448a09bbcb674724e61c23588f661499d
KangJuSeong <wntjd3686@gmail.com>
2022-11-08 14:29:42 +0900

    modify: MotorDriver.py code to class

--- MotorDriver.py
+++ MotorDriver.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-
-# 라즈베리파이 GPIO 패키지 
 import RPi.GPIO as GPIO
 from time import sleep
+
 
 # 모터 상태
 STOP  = 0
 FORWARD  = 1
-BACKWORD = 2
+BACKWARD= 2
 
 # 모터 채널
 CH1 = 0
@@ -32,78 +31,34 @@ IN2 = 13  #35 pin
 IN3 = 6   #31 pin
 IN4 = 5   #29 pin
 
-# 핀 설정 함수
-def setPinConfig(EN, INA, INB):        
-    GPIO.setup(EN, GPIO.OUT)
-    GPIO.setup(INA, GPIO.OUT)
-    GPIO.setup(INB, GPIO.OUT)
-    # 100khz 로 PWM 동작 시킴 
-    pwm = GPIO.PWM(EN, 100) 
-    # 우선 PWM 멈춤.   
-    pwm.start(0) 
-    return pwm
 
-# 모터 제어 함수
-def setMotorContorl(pwm, INA, INB, speed, stat):
-
-    #모터 속도 제어 PWM
-    pwm.ChangeDutyCycle(speed)  
+class Motor:
     
-    if stat == FORWARD:
-        GPIO.output(INA, HIGH)
-        GPIO.output(INB, LOW)
-        
-    #뒤로
-    elif stat == BACKWORD:
-        GPIO.output(INA, LOW)
-        GPIO.output(INB, HIGH)
-        
-    #정지
-    elif stat == STOP:
-        GPIO.output(INA, LOW)
-        GPIO.output(INB, LOW)
-
-        
-# 모터 제어함수 간단하게 사용하기 위해 한번더 래핑(감쌈)
-def setMotor(ch, speed, stat):
-    if ch == CH1:
-        #pwmA는 핀 설정 후 pwm 핸들을 리턴 받은 값이다.
-        setMotorContorl(pwmA, IN1, IN2, speed, stat)
-    else:
-        #pwmB는 핀 설정 후 pwm 핸들을 리턴 받은 값이다.
-        setMotorContorl(pwmB, IN3, IN4, speed, stat)
-  
-
-# GPIO 모드 설정 
-GPIO.setmode(GPIO.BCM)
-      
-#모터 핀 설정
-#핀 설정후 PWM 핸들 얻어옴 
-pwmA = setPinConfig(ENA, IN1, IN2)
-pwmB = setPinConfig(ENB, IN3, IN4)
-
+    def __init__(self, en, ina, inb):
+        self.pwm = self.setConfig(en, ina, inb)
+        self.ina = ina
+        self.inb = inb
+        self.en = en
     
-#제어 시작
+    @staticmethod
+    def setConfig(en, ina, inb):
+        GPIO.setup(en, GPIO.OUT)
+        GPIO.setup(ina, GPIO.OUT)
+        GPIO.setup(inb, GPIO.OUT)
+        pwm = GPIO.PWM(en, 100)
+        pwm.start(0)
+        return pwm
 
-# 앞으로 80프로 속도로
-setMotor(CH1, 80, FORWARD)
-setMotor(CH2, 80, FORWARD)
-#5초 대기
-sleep(5)
+    def controller(speed, stat):
+        self.pwm.ChangeDutyCycle(speed)
 
-# 뒤로 40프로 속도로
-setMotor(CH1, 40, BACKWORD)
-setMotor(CH2, 40, BACKWORD)
-sleep(5)
+        if stat == FORWARD:
+            GPIO.output(self.ina, HIGH)
+            GPIO.output(self.inb, LOW)
+        elif stat == BACKWARD:
+            GPIO.output(self.ina, LOW)
+            GPIO.output(self.inb, HIGH)
+        else:
+            GPIO.output(self.ina, LOW)
+            GPIO.output(self.inb, HIGH)
 
-# 뒤로 100프로 속도로
-setMotor(CH1, 100, BACKWORD)
-setMotor(CH2, 100, BACKWORD)
-sleep(5)
-
-#정지 
-setMotor(CH1, 80, STOP)
-setMotor(CH2, 80, STOP)
-
-# 종료
-GPIO.cleanup()
