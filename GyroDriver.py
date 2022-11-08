ad21c4dd047ec425b5568c356b04fd85532a09a4
KangJuSeong <wntjd3686@gmail.com>
2022-11-08 16:48:37 +0900

    add: test code and gyro get y_slope func

--- GyroDriver.py
+++ GyroDriver.py
@@ -1,22 +1,23 @@
 import smbus			#import SMBus module of I2C
 from time import sleep          #import
+import math
+
+
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+Device_Address = 0x68
 
 
 class Gyro:
-
-    PWR_MGMT_1   = 0x6B
-    SMPLRT_DIV   = 0x19
-    CONFIG       = 0x1A
-    GYRO_CONFIG  = 0x1B
-    INT_ENABLE   = 0x38
-    ACCEL_XOUT_H = 0x3B
-    ACCEL_YOUT_H = 0x3D
-    ACCEL_ZOUT_H = 0x3F
-    GYRO_XOUT_H  = 0x43
-    GYRO_YOUT_H  = 0x45
-    GYRO_ZOUT_H  = 0x47
-    Device_Address = 0x68
-    	
     def __init__(self):
         self.bus = smbus.SMBus(1) 
     	self.bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
@@ -37,15 +38,20 @@ class Gyro:
 
     def get_acc_data(self):
         return {
-                    'acc_x' : read_raw_data(ACCEL_XOUT_H)/16384.0,
-                    'acc_y' : read_raw_data(ACCEL_YOUT_H)/16384.0,
-                    'acc_z' : read_raw_data(ACCEL_ZOUT_H)/16384.0
+                    'acc_x' : self.read_raw_data(ACCEL_XOUT_H)/16384.0,
+                    'acc_y' : self.read_raw_data(ACCEL_YOUT_H)/16384.0,
+                    'acc_z' : self.read_raw_data(ACCEL_ZOUT_H)/16384.0
                 }
 
     def get_gyro_data(self):
         return {
-                    'gyro_x' : read_raw_data(GYRO_XOUT_H)/131,
-                    'gyro_y' : read_raw_data(GYRO_YOUT_H)/131,
-                    'gyro_z' : read_raw_data(GYRO_ZOUT_H)/131
+                    'gyro_x' : self.read_raw_data(GYRO_XOUT_H)/131,
+                    'gyro_y' : self.read_raw_data(GYRO_YOUT_H)/131,
+                    'gyro_z' : self.read_raw_data(GYRO_ZOUT_H)/131
                 }
+
+    def get_y_slope(self, x, y, z):
+        dist = math.sqrt((y*y) + (z*z))
+        radi = math.atan2(x, dist)
+        return math.degrees(radi)
     
--- main.py
+++ main.py
@@ -12,12 +12,27 @@ ENB = 0
 IN3 = 6
 IN4 = 5
 
-if __name__ == "__main__":
+def MotorTest(ENA, IN1, IN2, ENB, IN3, IN4):
     left = Motor(ENA, IN1, IN2)
     right = Motor(ENB, IN3, IN4)
  
     left.controller(100, Motor.FORWARD)
     right.controller(100, Motor.FORWARD)
     sleep(6)
+    left.controller(100, Motor.BACKWARD)
+    right.controller(100, Motor.BACKWARD)
+    sleep(6)
+    left.controller(0, Mortr.STOP)
+    right.controller(0, Motor.STOP)
     GPIO.cleanup()
+
+def GyroTest():
+    gyro = Gyro()
+    while True:
+        data = gyro.get_acc_data()
+        print(gyro.get_y_slope(data['acc_x'], data['acc_y'], data['acc_z']))
+
+if __name__ == "__main__":
+    GyroTest()
+    # MotorTest()
 
