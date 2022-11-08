74211935cfe2aa559362f736b33f5ff6f7eab855
KangJuSeong <wntjd3686@gmail.com>
2022-11-08 14:39:30 +0900

    modify: Motor class var

--- MotorDriver.py
+++ MotorDriver.py
@@ -1,39 +1,19 @@
 # -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
-from time import sleep
-
-
-# 모터 상태
-STOP  = 0
-FORWARD  = 1
-BACKWARD= 2
-
-# 모터 채널
-CH1 = 0
-CH2 = 1
-
-# PIN 입출력 설정
-OUTPUT = 1
-INPUT = 0
-
-# PIN 설정
-HIGH = 1
-LOW = 0
-
-# 실제 핀 정의
-#PWM PIN
-ENA = 26  #37 pin
-ENB = 0   #27 pin
-
-#GPIO PIN
-IN1 = 19  #37 pin
-IN2 = 13  #35 pin
-IN3 = 6   #31 pin
-IN4 = 5   #29 pin
 
 
 class Motor:
-    
+
+    STOP  = 0
+    FORWARD  = 1
+    BACKWARD= 2
+
+    OUTPUT = 1
+    INPUT = 0
+
+    HIGH = 1
+    LOW = 0
+
     def __init__(self, en, ina, inb):
         self.pwm = self.setConfig(en, ina, inb)
         self.ina = ina
@@ -58,7 +38,7 @@ class Motor:
         elif stat == BACKWARD:
             GPIO.output(self.ina, LOW)
             GPIO.output(self.inb, HIGH)
-        else:
+        elif stat == STOP:
             GPIO.output(self.ina, LOW)
-            GPIO.output(self.inb, HIGH)
+            GPIO.output(self.inb, LOW)
 
